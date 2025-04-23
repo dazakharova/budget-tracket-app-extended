3f961b58b68d4b7f865619bc9c0c6ee6b9f24086
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-04-23 19:06:58 +0300

    Add empty state message to TransactionList component

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -7,6 +7,7 @@ const TransactionList = () => {
     return (
         <div>
             <h3>Transactions</h3>
+            <p>{trackerCtx.transactions.length === 0 && 'no transactions yet'}</p>
             <ul id='transaction-list'>
             {trackerCtx.transactions && trackerCtx.transactions.map((tr, i) => <Transaction key={i} data={tr} />)}
             </ul>
