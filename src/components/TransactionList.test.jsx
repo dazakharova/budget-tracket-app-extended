f5c60078c96f04b427c29e1abb2de360fc189f10
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-04-23 18:57:05 +0300

    Add test to ensure TransactionList renders correct number of transactions passed as props

---
+++ src/components/TransactionList.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import { describe, test, expect } from 'vitest'
+import TransactionList from '../components/TransactionList'
+import { TrackerContext } from '../store/tracker-context'
+
+const modalRoot = document.createElement('div');
+modalRoot.setAttribute('id', 'modal');
+document.body.appendChild(modalRoot);
+
+const testTransactions = [
+  { id: 1, description: 'Groceries', amount: -25, category: 'food', type: 'expense' },
+  { id: 2, description: 'Salary', amount: 1000, category: 'salary', type: 'income' }
+]
+
+describe('TransactionList component', () => {
+  test('renders correct number of transactions', () => {
+    render(
+        <TrackerContext.Provider value={{ transactions: testTransactions }}>
+          <TransactionList />
+        </TrackerContext.Provider>
+    )
+
+    const listItems = screen.getAllByRole('listitem')
+    expect(listItems).toHaveLength(testTransactions.length)
+  })
+})
