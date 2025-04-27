2dbe3fefb81021eed14a545ce1b663d0b81bdbb2
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-04-28 02:38:34 +0300

    Update backend link

--- src/http.js
+++ src/http.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001/transactions';
+const BASE_URL = 'https://budget-tracket-app.onrender.com/transactions';
 
 export async function fetchAllTransactions() {
   const res = await fetch(BASE_URL);
